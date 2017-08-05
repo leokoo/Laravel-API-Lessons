9830bf6b9a0637338b67f095ad7d04b677bbcb23
Leo Koo <leokoo@gmail.com>
2017-08-06 02:28:06 +0800

    done with transformations

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -12,19 +12,18 @@ class LessonsController extends Controller {
 	 *
 	 * @return \Illuminate\Http\Response
 	 */
+
 	public function index() {
 		// really bad practice
 		// 1) All is bad
 		// 2) No way to attach meta data
 		// 3) Linking db structure to API output
 		// 4) No way to signal headers/response code
-
 		$lessons = Lesson::all();
 
 		return Response::json( [
-			'data' => $lessons->toArray()
+			'data' => $this->transformCollection( $lessons )
 		], 200 );
-
 	}
 
 	/**
@@ -57,18 +56,16 @@ class LessonsController extends Controller {
 	public function show( $id ) {
 		$lesson = Lesson::find( $id );
 
-
-
-		if ( ! $lesson )
-		{
+		if ( ! $lesson ) {
 			return Response::json( [
 				'error' => [
 					'message' => 'Lesson does not exist'
 				]
 			], 404 );
 		}
+
 		return Response::json( [
-			'data' => $lesson
+			'data' => $this->transform($lesson->toArray())
 		], 200 );
 	}
 
@@ -104,5 +101,18 @@ class LessonsController extends Controller {
 	 */
 	public function destroy( $id ) {
 		//
+	}
+
+// the transform method enables us to switch some_bool out later if needed
+	private function transformCollection( $lessons ) {
+		return array_map( [ $this, 'transform' ], $lessons->toArray() );
+	}
+
+	private function transform( $lesson ) {
+		return [
+			'title'  => $lesson['title'],
+			'body'   => $lesson['body'],
+			'active' => (boolean) $lesson['some_bool']
+		];
 	}
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -26,6 +26,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 $factory->define(App\Lesson::class, function (Faker\Generator $faker) {
 	return [
 		'title' => $faker->sentence(5),
-		'body'  => $faker->paragraph(4)
+		'body'  => $faker->paragraph(4),
+		'some_bool' => $faker->boolean()
 	];
 });
