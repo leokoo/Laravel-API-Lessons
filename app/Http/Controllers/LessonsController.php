a8cb4b5bb5578e388d969d43fdb61056395fb79f
Leo Koo <leokoo@gmail.com>
2017-08-05 15:22:34 +0800

    return error message when individual id not found

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -6,91 +6,103 @@ use App\Lesson;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
-class LessonsController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // really bad practice
-	    // 1) All is bad
-	    // 2) No way to attach meta data
-	    // 3) Linking db structure to API output
-	    // 4) No way to signal headers/response code
+class LessonsController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		// really bad practice
+		// 1) All is bad
+		// 2) No way to attach meta data
+		// 3) Linking db structure to API output
+		// 4) No way to signal headers/response code
 
-	    $lessons =  Lesson::all();
-	    return Response::json([
-	    	'data' => $lessons->toArray()
-	    ], 200);
+		$lessons = Lesson::all();
 
-    }
+		return Response::json( [
+			'data' => $lessons->toArray()
+		], 200 );
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store( Request $request ) {
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show( $id ) {
+		$lesson = Lesson::find( $id );
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+
+		if ( ! $lesson )
+		{
+			return Response::json( [
+				'error' => [
+					'message' => 'Lesson does not exist'
+				]
+			], 404 );
+		}
+		return Response::json( [
+			'data' => $lesson
+		], 200 );
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit( $id ) {
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update( Request $request, $id ) {
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy( $id ) {
+		//
+	}
 }
